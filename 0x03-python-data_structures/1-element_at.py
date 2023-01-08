b0775625622ce1f5a031c2eaebe758d7d4adb209
izunna <izunnavic@gmail.com>
2023-01-08 10:41:24 -0800

    added 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     for i in range(len(my_list)):
         if idx < 0:
             return "None"
-        if idx > len(my_list) - 1:
+        elif idx > (len(my_list) - 1):
             return "None"
         else:
-            return my_list[i]
+            return my_list[idx]
