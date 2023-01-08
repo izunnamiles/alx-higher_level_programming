8fc1fc1f5474a718a8a743e91238db90fed1be5d
izunna <izunnavic@gmail.com>
2023-01-08 21:41:12 +0100

    10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    new_array = []
     for i in range(len(my_list)):
-        if i % 2 == 0:
-            return True
+        if my_list[i] % 2 == 0:
+            new_array.append(True)
         else:
-            return False
+            new_array.append(False)
+    return (new_array)
