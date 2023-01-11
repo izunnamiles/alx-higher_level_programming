06c4e3d7e4b5bd5a13f74c1b64041dc52ee2c79f
izunna <izunnavic@gmail.com>
2023-01-11 23:46:58 +0100

    added 2-uniq_add.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    new_list = my_list[:]
-    for i in range(len(new_list)):
-        if new_list[i] == search:
-            new_list[i] = replace
-    return (new_list)
+def uniq_add(my_list=[]):
+    total = 0
+    for x in set(my_list):
+        total += x
+    return (total)
