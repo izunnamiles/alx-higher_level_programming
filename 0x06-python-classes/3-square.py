15f3fe7e664c0ea5c69b2498088dd190c5f17268
izunna <izunnavic@gmail.com>
2023-01-24 08:53:21 +0100

    added 3-square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a class square"""
+class Square:
+    """Square definition"""
+    def __init__(self, size=0):
+        """
+        Initialize the class Square
+
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    
+    def area(self):
+        """Return the area of the square"""
+        return pow(self.__size, 2)
