27524b2a15a07cc3c19da4f084a5315d4230ae05
izunna <izunnavic@gmail.com>
2023-01-24 18:20:42 +0100

    fix pycodestyle issues

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -17,7 +17,7 @@ class Square:
     def size(self):
         """Get/Set the current size of a square"""
         return (self.__size)
-    
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -26,11 +26,11 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+
     def area(self):
         """Return the area of the square"""
         return pow(self.__size, 2)
-    
+
     def __eq__(self, other):
         """Define the == comparision to a Square."""
         return self.area() == other.area()
