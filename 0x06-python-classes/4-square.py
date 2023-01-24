c79c5a52b4b410ffffc6b0170a3db41486129031
izunna <izunnavic@gmail.com>
2023-01-24 18:15:01 +0100

    fix pycodestyle issues

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -18,6 +18,7 @@ class Square:
         """Get/Set the current size of a square"""
         return (self.__size)
     @size.setter
+
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
@@ -25,6 +26,7 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def area(self):
         """Return the area of the square"""
         return pow(self.__size, 2)
