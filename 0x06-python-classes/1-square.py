b290adaa2539684a5131163e76563f23caf6d728
izunna <izunnavic@gmail.com>
2023-01-24 08:32:44 +0100

    added 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a class square"""
+class Square:
+    """Square definition"""
+    def __init__(self, size):
+        """
+        Initialize the class Square
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
