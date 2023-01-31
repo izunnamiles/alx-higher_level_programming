298c971f3b0099d0c8f02ac15737d428594bf48c
izunna <izunnavic@gmail.com>
2023-01-31 01:23:53 +0100

    updated doc block

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -3,8 +3,12 @@
 
 
 class Rectangle:
-    """Represent a rectangle."""
-    
+    """Represent a rectangle
+
+    Attributes:
+        number_of_instances (int): The number of Rectangle instances.
+    """
+
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -3,7 +3,12 @@
 
 
 class Rectangle:
-    """Represent a rectangle."""
+    """Represent a rectangle
+
+    Attributes:
+        number_of_instances (int): The number of Rectangle instances.
+        print_symbol (any): The symbol used for string representation.
+    """
     
     number_of_instances = 0
     print_symbol = "#"
