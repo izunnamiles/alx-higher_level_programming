1808523e5c9c7bcff0915b2b654f459a0f8df342
izunna <izunnavic@gmail.com>
2023-02-07 08:12:17 +0100

    added 8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a class-to-JSON function."""
+import json
+
+
+def class_to_json(obj):
+    """Return the dictionary represntation of a simple data structure."""
+    return obj.__dict__
