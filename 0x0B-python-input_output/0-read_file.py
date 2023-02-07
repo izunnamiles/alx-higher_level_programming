03e203f4248ede4e874c5fd088d4a20411148e2c
izunna <izunnavic@gmail.com>
2023-02-07 07:25:45 +0100

    added 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,9 @@
 
 
 def read_file(filename=""):
-    """Print the contents of a UTF8 text file to stdout."""
+    """Print the contents of a UTF8 text file to stdout.
+    Args:
+        filename (str): The name of the file to read.
+    """
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
